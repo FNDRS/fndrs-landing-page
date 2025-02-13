bf21050024909581915c6bc594827b5028e25b84
Carlos Pineda <cara_p@unicah.edu>
2025-02-13 07:47:12 -0600

    feat: improve header responsiveness and adjust layout in main section

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -11,7 +11,7 @@ export const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   return (
     <header className="bg-black text-white">
-      <div className="px-10 py-6 flex justify-center lg:justify-between items-center">
+      <div className="px-10 py-6 flex justify-center sm:justify-between items-center">
         <div className="flex-none self-start">
           <button className="self-start lg:hidden focus:outline-none" onClick={() => setIsMenuOpen(!isMenuOpen)}>
             <Menu className="w-6 h-6" />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,12 +18,12 @@ export default function Home() {
           <SharedIcon kind="arrowDown" className="mt-[2rem] cursor-pointer" />
         </div>
       </section>
-      <section className="h-screen">
-        <div className="xl:w-[1200px] mx-auto flex flex-col xl:flex-row items-center justify-center">
-          <div className="w-full xl:w-[800px] flex flex-col items-center xl:items-start text-center xl:text-left order-1 xl:order-1">
-            <h3 className="font-bold text-3xl">Who we are?</h3>
-            <h1 className="font-bold text-8xl">Revolutionaries</h1>
-            <p className="max-w-[500px] mt-4 text-lg leading-8">
+      <section className="h-screen flex justify-center">
+        <div className="xl:w-[1200px] flex flex-col xl:flex-row items-center justify-center xl:justify-between p-20 xl:p-0 xl:gap-24">
+          <div className="lg:w-full w-[400px] md:w-[600px] xl:w-[800px] flex flex-col items-center xl:items-start text-center xl:text-left order-1 xl:order-1">
+            <h3 className="self-start font-bold text-4xl sm:text-5xl md:text-6xl">Who we are?</h3>
+            <h1 className="self-start font-bold text-4xl sm:text-5xl md:text-6xl">Revolutionaries</h1>
+            <p className="mt-4 text-lg leading-8 text-justify">
               We are architects of change. Driven by vision and precision, we blend technology with purpose to craft solutions
               that leave a lasting mark. Our essence lies in pushing boundaries, redefining norms, and transforming potential into
               progress.
@@ -31,7 +31,7 @@ export default function Home() {
             <button className="bg-white hover:bg-gray-200 text-black py-2 px-12 rounded-xl mt-4 w-fit">Know Us</button>
           </div>
 
-          <div className="flex-1 h-[400px] order-2 xl:order-2">
+          <div className="order-2 w-[400px] md:w-[600px]">
             <SplineViewer scene={cardsAnimation} />
           </div>
         </div>
