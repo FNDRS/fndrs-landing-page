5d5ea89b172cc9a23d6a9cc6401dff40bd6aa09e
Marlon Geovany Castro <marlongeo1999@gmail.com>
2025-02-21 00:45:18 -0600

    feat: implement smooth entrance animations for main sections using framer-motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { motion } from "framer-motion"
+
 import { Footer } from "./components/footer"
 import { Header } from "./components/header"
 import { AboutUs } from "./components/main-sections/about-us"
@@ -7,6 +9,7 @@ import ContactUs from "./components/main-sections/contact-us"
 import { HeroSection } from "./components/main-sections/hero-section"
 import { OurGoal } from "./components/main-sections/our-goal"
 import { WhatWeDo } from "./components/main-sections/what-we-do"
+import { textVariants } from "./utils/text-animation"
 
 export default function Home() {
   const goToSection = (sectionId: string) => {
@@ -16,12 +19,58 @@ export default function Home() {
   return (
     <main>
       <Header goToSection={goToSection} />
-      <HeroSection />
-      <AboutUs />
-      <WhatWeDo />
-      <OurGoal />
-      <ContactUs />
-      <Footer goToSection={goToSection} />
+      <motion.section
+        initial="hidden"
+        whileInView="visible"
+        variants={textVariants}
+        transition={{ delay: 0.5 }}
+        viewport={{ once: false, amount: 0.1 }}>
+        <HeroSection />
+      </motion.section>
+
+      <motion.section
+        initial="hidden"
+        whileInView="visible"
+        variants={textVariants}
+        transition={{ delay: 0.5 }}
+        viewport={{ once: false, amount: 0.5 }}>
+        <AboutUs />
+      </motion.section>
+
+      <motion.section
+        initial="hidden"
+        whileInView="visible"
+        variants={textVariants}
+        transition={{ delay: 0.5 }}
+        viewport={{ once: false, amount: 0.5 }}>
+        <WhatWeDo />
+      </motion.section>
+
+      <motion.section
+        initial="hidden"
+        whileInView="visible"
+        variants={textVariants}
+        transition={{ delay: 0.5 }}
+        viewport={{ once: false, amount: 0.5 }}>
+        <OurGoal />
+      </motion.section>
+
+      <motion.section
+        initial="hidden"
+        whileInView="visible"
+        variants={textVariants}
+        transition={{ delay: 0.5 }}
+        viewport={{ once: false, amount: 0.5 }}>
+        <ContactUs />
+      </motion.section>
+      <motion.section
+        initial="hidden"
+        whileInView="visible"
+        variants={textVariants}
+        transition={{ delay: 0.5 }}
+        viewport={{ once: false, amount: 0.5 }}>
+        <Footer goToSection={goToSection} />
+      </motion.section>
     </main>
   )
 }
