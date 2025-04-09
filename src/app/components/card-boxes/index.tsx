a8d61d5126e692586604dd76383686a7a7acf1f9
Marlon Geovany Castro <marlongeo1999@gmail.com>
2025-04-09 09:15:52 -0600

    feat: update card styling and typography for improved readability and consistency

--- src/app/components/card-boxes/index.tsx
+++ src/app/components/card-boxes/index.tsx
@@ -26,44 +26,44 @@ export const CardBoxes = () => {
           <div
             onMouseEnter={() => setHoveredCard("card1")}
             onMouseLeave={() => setHoveredCard(null)}
-            className={`${cardScaleClass({ cardId: "card1" })} transition-transform duration-300 border-2 border-[#650208] w-full h-[300px] rounded-[35px] p-[2rem] relative`}>
+            className={`${cardScaleClass({ cardId: "card1" })} transition-transform duration-300 border-2 border-[#650208] w-full h-[300px] rounded-4xl p-8 relative`}>
             <SplineViewer scene={redParticles} className="absolute top-0 left-0 w-full h-full -z-10" />
-            <p className="max-w-[500px]">
+            <p className="text-lg">
               Smart solutions, fluid performance. We craft intuitive and scalable software that empowers businesses to grow and
               innovate effortlessly.
             </p>
-            <h2 className="text-[#CD0000] font-bold text-[40px] mt-8">Software Development</h2>
+            <h2 className="text-[#CD0000] font-bold text-3xl mt-8">Software Development</h2>
           </div>
           <div
             onMouseEnter={() => setHoveredCard("card2")}
             onMouseLeave={() => setHoveredCard(null)}
-            className={`${cardScaleClass({ cardId: "card2" })} transition-transform duration-300 border-2 border-[#DF9512] w-full h-[300px] rounded-[35px] p-4 relative`}>
+            className={`${cardScaleClass({ cardId: "card2" })} transition-transform duration-300 border-2 border-[#DF9512] w-full h-[300px] rounded-4xl relative p-8`}>
             <SplineViewer scene={orangeParticles} className="absolute top-0 left-0 w-full h-full -z-10" />
-            <h2 className="text-[#DF9512] font-bold text-xl">Branding & Identity</h2>
-            <p>
+            <p className="text-lg">
               More than a logo—branding is an experience. We shape identities that resonate, strategies that leave a lasting
               impression.
             </p>
+            <h2 className="text-[#DF9512] font-bold text-3xl mt-8">Branding & Identity</h2>
           </div>
         </div>
         <div className="flex flex-col w-full p-2 gap-4">
           <div
             onMouseEnter={() => setHoveredCard("card3")}
             onMouseLeave={() => setHoveredCard(null)}
-            className={`${cardScaleClass({ cardId: "card3" })} transition-transform duration-300 border-2 border-[#8A12DF] w-full h-[300px] rounded-[35px] p-6 relative`}>
+            className={`${cardScaleClass({ cardId: "card3" })} transition-transform duration-300 border-2 border-[#8A12DF] w-full h-[300px] rounded-4xl p-8 relative`}>
             <SplineViewer scene={purpleParticles} className="absolute top-0 left-0 w-full h-full -z-10" />
-            <p>Storytelling with purpose. We create content that connects and drives impact.</p>
-            <h2 className="text-[#8A12DF] font-bold text-lg mt-4">Content Strategy</h2>
+            <p className="text-lg">Storytelling with purpose. We create content that connects and drives impact.</p>
+            <h2 className="text-[#8A12DF] font-bold text-2xl mt-8">Content Strategy</h2>
           </div>
           <div
             onMouseEnter={() => setHoveredCard("card4")}
             onMouseLeave={() => setHoveredCard(null)}
-            className={`${cardScaleClass({ cardId: "card4" })} transition-transform duration-300 border-2 border-[#3557FF] w-full h-[300px] rounded-[35px] p-4 relative`}>
+            className={`${cardScaleClass({ cardId: "card4" })} transition-transform duration-300 border-2 border-[#3557FF] w-full h-[300px] rounded-4xl p-8 relative`}>
             <SplineViewer scene={blueParticles} className="absolute top-0 left-0 w-full h-full -z-10" />
-            <h2 className="text-[#3557FF] font-bold text-lg mb-4">Web Development</h2>
             <p>
               Elevating brands through high-performance websites. From sleek designs to robust functionality, we craft digital
               experiences that captivate and convert.
+              <h2 className="text-[#3557FF] font-bold text-2xl mt-8">Web Development</h2>
             </p>
           </div>
         </div>
